ac3f6204ddca181c35e18504f48ae20781fcb4c2
acheronfail <acheronfail@gmail.com>
2018-02-08 13:05:45 +1100

    support non-monospaced characters, eg, emoji, unicode, etc

--- vscode_column_selection.py
+++ vscode_column_selection.py
@@ -23,50 +23,67 @@ class ColumnSelectionCommand(sublime_plugin.TextCommand):
         #   ACE:        the last selection made
         views_selections = self.view.sel()
         primary_selection = views_selections[0]
-        anchor = self.view.rowcol(primary_selection.a)
 
-        # Calculate head position from click event.
-        window_vector = (event.get('x'), event.get('y'))
-        head_point = self.view.window_to_text(window_vector)
-        head_row = self.view.rowcol(head_point)[0]
-        layout_vector = self.view.window_to_layout(window_vector)
-        head = (head_row, layout_vector[0] // self.view.em_width())
+        # Use layout coordinates to find the start and end of each selection on
+        # each line. It's easier just to use `row`s and `col`s, but that won't
+        # be accurate for emoji and other non-monospaced characters.
+        # Think of `(x1, y1)` and `(x2, y2)` defining the edges of a rectangle
+        # which is our selection.
+        (x1, y1) = self.view.text_to_layout(primary_selection.a)
+        (x2, y2) = self.view.window_to_layout((event.get('x'), event.get('y')))
+        start_row = self.view.rowcol(primary_selection.a)[0]
+        click_row = self.view.rowcol(self.view.layout_to_text((x2, y2)))[0]
 
-        # Place a selection on every line if:
-        #   anchor.col == head.col, or
-        #   anchor line is empty.
-        col_start, col_end = anchor[1], head[1]
-        all_lines = col_start == col_end or self.view.line(primary_selection).empty()
+        # Check if we should put a selection of every line.
+        all_empty = True
+        skipped_lines = []
 
-        # Iterate through lines and create selection regions.
+        # Threshold for ignoring selections - used to allow for the effects that
+        # non-monospaced characters have on lines.
+        threshold = self.view.em_width() / 2
+
+        # Iterate through lines (from top to bottom) and create selection regions.
         selections = []
-        start, end = min(anchor[0], head[0]), max(anchor[0], head[0])
-        for row in range(start, end + 1):
+        first, last = min(start_row, click_row), max(start_row, click_row)
+        for row in range(first, last + 1):
+            # This line's region.
             line = self.view.line(self.view.text_point(row, 0))
 
-            # Either skip empty lines, or add a selection if `all_lines` is set.
+            # Just remember the line's region if it's empty and continue.
             if line.empty():
-                if all_lines:
-                    selections.append(line.a)
+                skipped_lines.append(line)
                 continue
 
-            # Find text points for selection.
-            a = clamp(self.view.text_point(row, col_start), line.a, line.b)
-            b = clamp(self.view.text_point(row, col_end), line.a, line.b)
+            # Find the first & last char at the start & click points on this line.
+            line_y = self.view.text_to_layout(line.a)[1]
+            a = clamp(self.view.layout_to_text((x1, line_y)), line.a, line.b)
+            b = clamp(self.view.layout_to_text((x2, line_y)), line.a, line.b)
+            region = sublime.Region(a, b)
 
-            # Ignore lines that don't reach inside the column selection.
-            if col_start < col_end and self.view.rowcol(a)[1] < col_start:
+            # Skip lines that don't reach inside the column selection.
+            if x1 < x2 and self.view.text_to_layout(a)[0] < x1 - threshold:
+                skipped_lines.append(region)
                 continue
-            elif col_start > col_end and self.view.rowcol(a)[1] < col_end:
+            elif x1 > x2 and self.view.text_to_layout(a)[0] < x2 - threshold:
+                skipped_lines.append(region)
                 continue
 
-            selections.append(sublime.Region(a, b))
+            if all_empty and not region.empty():
+                all_empty = False
+
+            # Add region to selections.
+            selections.append(region)
+
+        # Place a selection on every line (even if it's not within the rect) if:
+        #   the starting line is an empty line, or
+        #   all of the selected regions are empty.
+        if self.view.line(primary_selection).empty() or all_empty:
+            selections.extend(skipped_lines)
 
         # Replace the view's selections, if we have any to add.
         if len(selections):
             views_selections.clear()
             views_selections.add_all(selections)
-
 
     # Return true here in order to get mouse event passed to run
     def want_event(self):
